ef06a54d07721a7137e78370ccab3677c9a1dc06
Ayush Shukla <ayushukla002@gmail.com>
2023-11-01 19:47:04 +0530

    create first react project via vite

---
+++ firstproject/src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./nav"
+function App() {
+
+  return (
+    <>
+    <h1>Ayush Shukla</h1>
+    <Nav/>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem tenetur quam minima.</p>
+    </>
+  )
+}
+
+export default App
---
+++ firstproject/src/nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+    return(
+        <h2>Arpit</h2>
+    )
+}
+
+export default Nav
